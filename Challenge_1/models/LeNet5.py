29e0e1cf56d27cfea1291d77e53360cd7473f1ad
niccolozanieri <niccolo.zanieri.13@gmail.com>
2024-05-02 11:47:09 +0200

    feat: v1 operative LeNet5Nb.ipynb

--- Challenge_1/models/LeNet5.py
+++ Challenge_1/models/LeNet5.py
@@ -4,16 +4,19 @@ import torchvision.transforms as transforms
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
+from sklearn.metrics import roc_curve, auc
+import matplotlib.pyplot as plt
 
 class LeNet5(nn.Module):
     def __init__(self):
         super().__init__()
         self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
+        self.pool = nn.MaxPool2d(2, 2) 
         self.conv2 = nn.Conv2d(6, 16, 5)
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
         self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.fc3 = nn.Linear(84, 1)
+        self.sigmoid = torch.nn.Sigmoid()
     
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
@@ -27,13 +30,13 @@ class LeNet5(nn.Module):
     def training_step(self, batch):
         images, labels = batch
         out = self.forward(images)
-        loss = torch.nn.functional.cross_entropy(input=out, target=labels)
+        loss = torch.nn.BCEWithLogitsLoss()(out, labels.unsqueeze(1).float())
         return loss
     
     def validation_step(self, batch):
         images, labels = batch
         out = self.forward(images)
-        loss = torch.nn.functional.cross_entropy(input=out, target=labels)
+        loss = torch.nn.BCEWithLogitsLoss()(out, labels.unsqueeze(1).float())
         return loss
     
     def train_model(self, train_dataloader, val_dataloader, epochs=10, lr=0.001, device=None, wandb=None, freeze=False):
@@ -42,6 +45,12 @@ class LeNet5(nn.Module):
         train_losses = []
         val_losses = []
         print("Training on: " + str(device))
+
+        aucs = []
+        best_fpr = []
+        best_tpr = []
+        best_auc = 0
+
         for epoch in range(epochs):
             # Training Phase with gpu
             self.train()
@@ -56,11 +65,29 @@ class LeNet5(nn.Module):
             # Validation phase with gpu
             lr_scheduler.step()
             self.eval()
+            full_val = []
+
             for batch in val_dataloader:
                 _, images, labels = batch
+                full_val.append((images, labels))
                 images, labels = images.to(device), labels.to(device)
                 loss_val = self.validation_step((images, labels))
                 val_losses.append(loss_val.item())
+            
+            # Metrics evaluation
+            full_val = torch.cat([images for images, _ in full_val]), torch.cat([labels for _, labels in full_val])
+            images, labels = full_val
+            images, labels = images.to(device), labels.to(device)
+            out = self.sigmoid(self.forward(images))
+            fpr, tpr, _ = roc_curve(labels.cpu().detach(), out.cpu().detach())
+            roc_auc = auc(fpr, tpr)
+
+            if roc_auc > best_auc:
+                best_auc = roc_auc
+                best_fpr = fpr
+                best_tpr = tpr
+            aucs.append(roc_auc)
+                
             print("Epoch: " + str(epoch) + " Loss train: " + str(np.average(train_losses)))
             print("Epoch: " + str(epoch) + " Loss val: " + str(np.average(val_losses)))
             if wandb:
@@ -69,6 +96,15 @@ class LeNet5(nn.Module):
             if epoch + 1 % 5 == 0:
                 torch.save(self.state_dict(), "./weights/lenet5_model.pth")
         torch.save(self.state_dict(), "./weights/lenet5_model.pth")
+
+        
+        
+        print(f'Best AUC: {best_auc}')
+        fig, ax = plt.subplots()
+        ax.plot(best_fpr, best_tpr)
+        ax.set_title("Best ROC")
+
+        plt.show()
     
     def predict_model(self, images, device, path=None):
         self.eval()  # Set the model to evaluation mode
@@ -77,7 +113,7 @@ class LeNet5(nn.Module):
             for batch in images:
                 img_name, image, _ = batch
                 image = image.to(device)
-                out = self.resnet(image)
+                out = self.forward(image)
                 outputs.append((img_name,torch.nn.functional.softmax(out, dim=1)))
             # from Nx(32x1) to a list of tuples (img_name, prediction)
             final_outputs=[]
@@ -85,7 +121,7 @@ class LeNet5(nn.Module):
                 # tuple of tensors
                 img_names, predictions = output
                 for img_name, prediction in zip(img_names, predictions):
-                    final_outputs.append((img_name, torch.argmax(prediction).item()))
+                    final_outputs.append((img_name, torch.round(self.sigmoid(prediction))))
         if path:
             torch.save(str(outputs), path)  # Optionally save the predictions
         return final_outputs
